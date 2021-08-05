617c1890ea845083e7be4ecba39b5fe1c3d99ad4
Nick Foden <nickfoden@gmail.com>
2021-08-05 13:01:43 -0400

    update shape start category filters (#1)

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -1,9 +1,21 @@
-import { getAllVideos } from "../adapters/fauna";
+import { getCategoryOfVideos, getAllVideos } from "../adapters/fauna";
 
 const resolvers = {
   Query: {
     videos: async () => {
       const result = await getAllVideos();
+      return result;
+    },
+    opinion: async () => {
+      const result = await getCategoryOfVideos();
+      return result;
+    },
+    diaryOfSong: async () => {
+      const result = await getCategoryOfVideos();
+      return result;
+    },
+    opDocs: async () => {
+      const result = await getCategoryOfVideos();
       return result;
     },
   },
